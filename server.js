a250ae87194ce321ba94eafd8fe00e355e81279d
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-10-26 15:07:17 -0400

    add wildcard for 404 page

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/feedback', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
 );
 
-app.get('/*', (req, res) => res.sendFile(path.))
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, `/public/pages/notfound.html`)))
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
